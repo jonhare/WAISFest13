54f17e9f3b800b95cdb660b388f91cdadef4ce5e
Dave Dupplaw <dpd@ecs.soton.ac.uk>
2013-09-12 15:01:01 +0100

    Added distance/volume to MIDI sound translator

--- FlickrVisualisations/src/main/java/uk/ac/soton/ecs/wais/fest13/sound/midi/MIDISoundTranslator.java
+++ FlickrVisualisations/src/main/java/uk/ac/soton/ecs/wais/fest13/sound/midi/MIDISoundTranslator.java
@@ -39,6 +39,9 @@ public class MIDISoundTranslator implements SoundTranslator
 	/** The pan controller - to set where in the stereo field a note is played */
 	public int PAN_CONTROLLER = 10;
 	
+	/** The volume controller - to audibly represent the distance from our user */
+	public int VOLUME_CONTROLLER = 7;
+	
 	/**
 	 * 	Default constructor
 	 *	@throws MidiUnavailableException If a synth could not be created
@@ -74,7 +77,11 @@ public class MIDISoundTranslator implements SoundTranslator
 		
 		// Set the pan position
 		chan.controlChange( PAN_CONTROLLER, 
-				getPanPosition( comment.location.longitude ) );
+			getPanPosition( comment.location.longitude ) );
+		
+		// Set the volume based on the distance from the observer
+		chan.controlChange( VOLUME_CONTROLLER, 
+			getUserDistanceVolume( comment.location, userInformation.location ) );
 		
 		// Play the note
 		chan.noteOn( note.noteNumber, 100 );
@@ -96,6 +103,35 @@ public class MIDISoundTranslator implements SoundTranslator
 	{
 		return (int)((longitude+180)*127/360);
 	}
+	
+	/**
+	 * 	Converts the user's distance into a volume value 0 - 127
+	 * 
+	 *	@param comment The comment geo location
+	 *	@param userLocation The user geo location
+	 *	@return The volume
+	 */
+	public int getUserDistanceVolume( GeoLocation comment, GeoLocation userLocation )
+	{
+		// This distance calculation is ported from:
+		// http://www.movable-type.co.uk/scripts/latlong.html
+		double R = 6371; // km
+		double dLat = Math.toRadians( userLocation.latitude-comment.latitude );
+		double dLon = Math.toRadians( userLocation.longitude-comment.longitude );
+		double lat1 = Math.toRadians( comment.latitude );
+		double lat2 = Math.toRadians( userLocation.latitude );
+
+		double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+		double d = R * c;
+		
+		// The maximum great circle distance is just over 20000km, so we'll
+		// divide by that and multiply by the maximum our volume can be
+		int volume = (int)(d * 127 / 20020);
+		
+		return volume;
+	}
 
 	/**
 	 *	{@inheritDoc}
@@ -114,13 +150,16 @@ public class MIDISoundTranslator implements SoundTranslator
 	 */
 	public static void main( String[] args ) throws MidiUnavailableException, InterruptedException
 	{		
+		// Put our pretend user in London
+		UserInformation userInformation = new UserInformation();
+		userInformation.location = new GeoLocation( 51.507222, -0.1275 );
+
+		// Create 20 random social comments
 		MIDISoundTranslator mst = new MIDISoundTranslator();
-		
 		for( int i = 0; i < 20; i++ )
 		{
 			SocialComment comment = new SocialComment();
 			comment.location = new GeoLocation( Math.random()*90, Math.random()*360-180 );
-			UserInformation userInformation = new UserInformation();
 			
 			mst.translate( comment, userInformation );
 			Thread.sleep( 600 );
