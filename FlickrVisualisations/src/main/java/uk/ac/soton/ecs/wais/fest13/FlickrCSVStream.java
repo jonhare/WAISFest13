a4e7ae470ae35604fdd1584bb02c286e667af64d
Sina Samangooei <sinjax@gmail.com>
2013-09-12 17:09:05 +0100

    vis and sound

--- FlickrVisualisations/src/main/java/uk/ac/soton/ecs/wais/fest13/FlickrCSVStream.java
+++ FlickrVisualisations/src/main/java/uk/ac/soton/ecs/wais/fest13/FlickrCSVStream.java
@@ -6,9 +6,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.NoSuchElementException;
 
+import javax.sound.midi.MidiUnavailableException;
 import javax.swing.JFrame;
 
 import org.openimaj.image.DisplayUtilities;
@@ -21,11 +24,58 @@ import org.openimaj.util.function.Operation;
 import org.openimaj.util.stream.AbstractStream;
 import org.openimaj.util.stream.Stream;
 
+import uk.ac.soton.ecs.jsh2.mediaeval13.placing.evaluation.GeoLocation;
 import uk.ac.soton.ecs.sound.vis.FlickrTagFilter;
 import uk.ac.soton.ecs.sound.vis.FlickrTimePostedWindow;
 import uk.ac.soton.ecs.sound.vis.FlickrTimePredicate;
+import uk.ac.soton.ecs.wais.fest13.sound.SoundTranslator;
+import uk.ac.soton.ecs.wais.fest13.sound.midi.MIDISoundTranslator;
 
 public class FlickrCSVStream extends AbstractStream<Context> {
+	private static final class FlickrImageSoundOperation implements
+			Operation<Context> {
+		private final List<SocialComment> comments;
+
+		private FlickrImageSoundOperation(List<SocialComment> comments) {
+			this.comments = comments;
+		}
+
+		@Override
+		public void perform(Context object) {
+			
+			comments.add(contextToSocial(object));
+		}
+
+		private SocialComment contextToSocial(Context object) {
+			SocialComment ret = new SocialComment();
+			ret.location = new GeoLocation((Double)object.getTyped(FlickrCSVStream.LATITUDE), (Double)object.getTyped(FlickrCSVStream.LONGITUDE));
+			return ret;
+		}
+	}
+
+	private static final class FlickrImageDrawOperation implements
+			Operation<Context> {
+		private final FImage img;
+
+		private FlickrImageDrawOperation(FImage img) {
+			this.img = img;
+		}
+
+		@Override
+		public void perform(Context ctx) {
+			final double x = (Double) ctx.get(LONGITUDE) + 180;
+			final double y = 90 - (Double) ctx.get(LATITUDE);
+
+			final int xx = (int) (x * (1.0 * img.getWidth() / 360));
+			final int yy = (int) (y * (1.0 * (img.getHeight() - 40) / 180));
+
+			if (xx >= 0 && xx < img.getWidth() && yy >= 0 && yy < img.getHeight()) {
+				img.pixels[yy][xx] = 1;
+				img.drawPoint(new Point2dImpl(xx, yy), 1f, 3);
+			}
+		}
+	}
+
 	public static final String FLICKR_ID = "flickrId";
 	public static final String USER_ID = "userId";
 	public static final String URL = "url";
@@ -85,18 +135,19 @@ public class FlickrCSVStream extends AbstractStream<Context> {
 		return ctx;
 	}
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, MidiUnavailableException {
 		final FImage img = new FImage(1080, 580);
 
 		final JFrame wind = DisplayUtilities.displaySimple(img);
 
 		final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
-		final String data = "/Users/jon/Data/data-takensort.csv";
+//		final String data = "/Users/jon/Data/data-takensort.csv";
 		// final String data = "/home/dd/data-takensort.csv";
-		// String data =
-		// "/Users/ss/Development/java/WAISFest13/data/data-10000.csv";
-
+		 String data =
+		 "/Users/ss/Development/java/WAISFest13/data-taken.csv";
+		final List<SocialComment> comments = new ArrayList<SocialComment>();
+		final SoundTranslator trans = new MIDISoundTranslator();
 		new FlickrCSVStream(new File(data))
 				.filter(new FlickrTimePredicate())
 				.transform(new FlickrTimePostedWindow(24 * 60 * 60 * 1000L))
@@ -105,25 +156,17 @@ public class FlickrCSVStream extends AbstractStream<Context> {
 					@Override
 					public void perform(Context object) {
 						img.multiplyInplace(0.99f);
-
+						comments.clear();
+						Operation<Context> op = new FlickrImageDrawOperation(img);
 						((Stream<Context>) object.get("window"))
 								.filter(new FlickrTagFilter("snow"))
-								.forEach(new Operation<Context>() {
-									@Override
-									public void perform(Context ctx) {
-										final double x = (Double) ctx.get(LONGITUDE) + 180;
-										final double y = 90 - (Double) ctx.get(LATITUDE);
-
-										final int xx = (int) (x * (1.0 * img.getWidth() / 360));
-										final int yy = (int) (y * (1.0 * (img.getHeight() - 40) / 180));
-
-										if (xx >= 0 && xx < img.getWidth() && yy >= 0 && yy < img.getHeight()) {
-											img.pixels[yy][xx] = 1;
-											img.drawPoint(new Point2dImpl(xx, yy), 1f, 3);
-										}
-									}
-								});
-
+								.filter(new PassThrough<Context>(new FlickrImageDrawOperation(img)))
+								.filter(new PassThrough<Context>(new FlickrImageSoundOperation(comments)))
+								.forEach(new GetAll<Context>());
+						UserInformation userInformation = new UserInformation();
+						userInformation = new UserInformation();
+						userInformation.location = new GeoLocation(51.5,0);
+						trans.translate(comments, userInformation);
 						img.drawShapeFilled(new Rectangle(0, 540, 1080, 40), 0f);
 						img.drawText(df.format(new Date((Long) object.get("start"))), 0, 580,
 								HersheyFont.ROMAN_SIMPLEX, 18, 1f);
---
+++ FlickrVisualisations/src/main/java/uk/ac/soton/ecs/wais/fest13/PassThrough.java
@@ -0,0 +1,20 @@
+package uk.ac.soton.ecs.wais.fest13;
+
+import org.openimaj.util.function.Operation;
+import org.openimaj.util.function.Predicate;
+
+public class PassThrough<T> implements Predicate<T> {
+
+	private Operation<T> op;
+
+	public PassThrough(Operation<T> op) {
+		this.op = op;
+	}
+
+	@Override
+	public boolean test(T object) {
+		op.perform(object);
+		return true;
+	}
+
+}
