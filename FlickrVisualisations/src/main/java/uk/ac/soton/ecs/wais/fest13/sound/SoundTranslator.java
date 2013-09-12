8078d963f4d5938fcdf69975fd16113d35af84c7
Dave Dupplaw <dpd@ecs.soton.ac.uk>
2013-09-12 11:55:43 +0100

    Added some encapsulation classes and sound translator API

---
+++ FlickrVisualisations/src/main/java/uk/ac/soton/ecs/wais/fest13/sound/SoundTranslator.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package uk.ac.soton.ecs.wais.fest13.sound;
+
+import uk.ac.soton.ecs.wais.fest13.SocialComment;
+import uk.ac.soton.ecs.wais.fest13.UserInformation;
+
+/**
+ *	Takes a social comment and plays an appropriate sound.
+ *
+ *	@author David Dupplaw (dpd@ecs.soton.ac.uk)
+ *  @created 12 Sep 2013
+ */
+public interface SoundTranslator
+{
+	/**
+	 * 	For the given social comment and the given information about the user,
+	 * 	this sound translator should generate an appropriate sound.
+	 * 
+	 *	@param comment The social comment.
+	 *	@param userInformation The user information.
+	 */
+	public void translate( SocialComment comment, UserInformation userInformation ); 
+}
